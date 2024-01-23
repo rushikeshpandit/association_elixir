deb308fd8b886d65ced8ef6fa715d5e6640951c1
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-01-23 17:20:26 +0530

    - User and session controller tests

---
+++ test/association_elixir_web/controllers/sessions_controller_test.exs
@@ -0,0 +1,76 @@
+defmodule AssociationElixirWeb.SessionsControllerTest do
+  use AssociationElixirWeb.ConnCase
+  alias AssociationElixir.Shared.Tokenr
+
+  test "throw an error when user is not authenticated", %{conn: conn} do
+    conn =
+      post(
+        conn,
+        Routes.api_session_path(conn, :me, token: "dsfdsfsd")
+      )
+
+    assert json_response(conn, 401)["error"] == "Invalid/Unauthenticated token"
+  end
+
+  describe "handle with sessions" do
+    setup :include_normal_user_token
+
+    test "create session", %{conn: conn, user: user, password: password} do
+      conn =
+        post(
+          conn,
+          Routes.api_session_path(conn, :create, email: user.email, password: password)
+        )
+
+      assert json_response(conn, 201)["data"]["user"]["data"]["email"] == user.email
+    end
+
+    test "get me", %{conn: conn, user: user, token: token} do
+      conn =
+        post(
+          conn,
+          Routes.api_session_path(conn, :me, token: token)
+        )
+
+      assert json_response(conn, 200)["data"]["user"]["data"]["email"] == user.email
+    end
+
+    test "reset_password", %{conn: conn, user: user} do
+      token = Tokenr.generate_forgot_email_token(user)
+
+      conn =
+        post(
+          conn,
+          Routes.api_session_path(conn, :reset_password,
+            token: token,
+            user: %{password: "adm@elxpro.coM1", password_confirmation: "adm@elxpro.coM1"}
+          )
+        )
+
+      assert json_response(conn, 200)["data"]["user"]["data"]["email"] == user.email
+    end
+
+    test "throw error when try to reset_password", %{conn: conn} do
+      conn =
+        post(
+          conn,
+          Routes.api_session_path(conn, :reset_password,
+            token: "fddsfs",
+            user: %{password: "adm@elxpro.coM1", password_confirmation: "adm@elxpro.coM1"}
+          )
+        )
+
+      assert json_response(conn, 400)["message"] == "Invalid token"
+    end
+
+    test "forgot password", %{conn: conn, user: user} do
+      conn =
+        post(
+          conn,
+          Routes.api_session_path(conn, :forgot_password, email: user.email)
+        )
+
+      assert response(conn, 204) == ""
+    end
+  end
+end
---
+++ test/association_elixir_web/controllers/user_controller_test.exs
@@ -0,0 +1,21 @@
+defmodule AssociationElixirWeb.UserControllerTest do
+  use AssociationElixirWeb.ConnCase
+
+  import AssociationElixir.UserFixtures
+
+  setup :include_normal_user_token
+
+  test "create user when data is valid", %{conn: conn} do
+    attrs = user_attrs()
+    conn = post(conn, Routes.api_user_path(conn, :create, user: attrs))
+    assert %{"id" => id} = json_response(conn, 201)["data"]
+
+    conn = get(conn, Routes.api_user_path(conn, :show, id))
+    email = String.downcase(attrs.email)
+
+    assert %{
+             "id" => ^id,
+             "email" => ^email
+           } = json_response(conn, 200)["data"]
+  end
+end
