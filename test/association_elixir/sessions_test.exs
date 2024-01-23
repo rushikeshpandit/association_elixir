d26a715fcab481d1117e4ceded548d25e75f3a8c
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-01-23 18:41:33 +0530

    - Added tests for sessions

---
+++ test/association_elixir/sessions_test.exs
@@ -0,0 +1,38 @@
+defmodule AssociationElixir.SessionsTest do
+  use AssociationElixir.DataCase
+
+  alias AssociationElixir.Sessions
+  import AssociationElixir.UserFixtures
+
+  test "get user from token" do
+    user = user_fixture()
+    password = "Rushi@7588"
+    assert {:ok, returned_user, token} = Sessions.create(user.email, password)
+
+    assert {:ok, returned_user} == Sessions.me(token)
+  end
+
+  test "return authenticated user" do
+    user = user_fixture()
+    password = "Rushi@7588"
+    assert {:ok, returned_user, _token} = Sessions.create(user.email, password)
+
+    assert user.email == returned_user.email
+  end
+
+  test "throw error when wrong password entered" do
+    user = user_fixture()
+    password = "Rushi@1431431"
+    assert {:error, message} = Sessions.create(user.email, password)
+
+    assert "Email or password is incorrect" == message
+  end
+
+  test "throw error if user does not exists" do
+    user = user_fixture()
+    password = "Rushi@1431431"
+    assert {:error, message} = Sessions.create(user.email, password)
+
+    assert "Email or password is incorrect" == message
+  end
+end
