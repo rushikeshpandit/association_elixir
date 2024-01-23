4d347e9ccf7f142179a3668801e3b48c6e11cb71
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-01-23 18:41:04 +0530

    - Added fallback controller and changeset json

---
+++ lib/association_elixir_web/controllers/fallback_controller.ex
@@ -0,0 +1,23 @@
+defmodule AssociationElixirWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use AssociationElixirWeb, :controller
+
+  # This clause handles errors returned by Ecto's insert/update/delete.
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(json: AssociationElixirWeb.ChangesetJSON)
+    |> render(:error, changeset: changeset)
+  end
+
+  # This clause is an example of how to handle resources that cannot be found.
+  def call(conn, {:error, message}) do
+    conn
+    |> put_status(:bad_request)
+    |> json(%{message: message})
+  end
+end
