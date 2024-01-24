e5b39036d3cd27e3e937544f3625b296570ea2c9
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-01-24 17:35:54 +0530

    - Updated code to check authenticated user

--- lib/association_elixir_web/controllers/middleware/ensure_authenticated.ex
+++ lib/association_elixir_web/controllers/middleware/ensure_authenticated.ex
@@ -7,7 +7,7 @@ defmodule AssociationElixirWeb.Middleware.EnsureAuthenticated do
   def call(conn, _opts) do
     with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
          {:ok, user} = Tokenr.verify_auth_token(token),
-         true <- user.role in [:USER, :ADMIN, :SUPER_ADMIN] do
+         true <- Enum.member?([:USER, :ADMIN, :SUPER_ADMIN], user.role) do
       put_req_header(conn, "user_id", user.id)
     else
       _ ->
